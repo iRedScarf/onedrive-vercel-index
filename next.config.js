b4ef57568e860afd503b09a7489543fe6bf7e3e9
Rock Star <45270927+therockstarind@users.noreply.github.com>
2023-07-09 16:25:29 +0530

    feat: RM NEXT_PUBLIC use @next/bundle-analyzer for env and add basic demo.env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 const { i18n } = require('./next-i18next.config')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+  openAnalyzer: false,
+})
 
-module.exports = {
+module.exports = withBundleAnalyzer({
+  env: {
+    USER_PRINCIPLE_NAME: process.env.USER_PRINCIPLE_NAME,
+    SITE_TITLE: process.env.SITE_TITLE,
+    BASE_DIRECTORY: process.env.BASE_DIRECTORY,
+    EMAIL: process.env.EMAIL
+  },
   i18n,
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
-  trailingSlash: true
-}
+  trailingSlash: true,
+})
